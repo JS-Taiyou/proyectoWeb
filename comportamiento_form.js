38614d480c07cd66b39851c2cb6f0b984ed61fc4
Jorge Solís <jpsb94@comunidad.unam.mx>
2017-05-29 13:14:46 -0500

    Formulario en una sola página

---
+++ comportamiento_form.js
@@ -0,0 +1,40 @@
+
+indice = 0;
+
+
+$("#siguiente").click(function(){
+    if(indice >= 0 && indice <=1){
+        $(".pagina".concat(indice)).addClass('hidden');
+        indice += 1;
+        if(indice === 1){
+            $("#anterior").removeClass('hidden');
+            $("#titulo").text('Evaluación de riesgos');
+        }
+        $(".pagina".concat(indice)).removeClass('hidden');
+        if(indice === 2){
+            $("#siguiente").addClass('hidden');
+            $("#guardar").removeClass('hidden');
+            $("#titulo").text("Aprobación de medicamentos");
+        }
+    }
+});
+
+$("#anterior").click(function () {
+        $(".pagina".concat(indice)).addClass('hidden');
+        indice -= 1;
+        if(indice === 1){
+            $("#siguiente").removeClass('hidden');
+            $("#guardar").addClass('hidden');
+            $("#titulo").text('Evaluación de riesgos');
+        }
+        $(".pagina".concat(indice)).removeClass('hidden');
+        if(indice === 0){
+            $("#anterior").addClass('hidden');
+            $("#titulo").text('Datos generales');
+        }
+
+});
+
+$("#guardar").click(function () {
+    console.log($( "#formulario" ).serialize());
+});
